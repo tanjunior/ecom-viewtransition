2095da2ea1614db6015021dd12b4cdcfdd35688a
Spastic <junior.tan@live.com>
2024-02-21 16:03:07 +0800

    cleanup

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -15,19 +15,13 @@ export function Card({ id, name, artist, imageUrl }: CardProps) {
         className="text-black hover:text-pink-500"
         unstable_viewTransition
       >
-        <div className="shadow-md hover:shadow-lg relative">
+        <div className="relative shadow-md hover:shadow-lg">
           <img
-            className="card-image rounded-md relative z-10 c-card--album"
+            className="relative z-10 rounded-md card-image c-card--album"
             src={imageUrl}
             alt={name}
             width="400"
             height="400"
-          />
-          <img
-            src="/react-router-records/vinyl-lp.webp"
-            width="400"
-            height="400"
-            className="absolute top-0 opacity-0 vinyl-image c-card--vinyl"
           />
         </div>
         <p className="pt-4 font-semibold">{name}</p>
