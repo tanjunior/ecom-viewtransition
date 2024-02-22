8dfcd4b9326e73efb6a74574d5d385438e73f416
Spastic <junior.tan@live.com>
2024-02-22 23:35:23 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Layout from "./routes/Layout";
 import HomeComponent from "./routes/Home";
-import Product from "@/routes/Product"
+import ProductComponent from "@/routes/Product";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { homeLoader, productLoader } from "./lib/loaders";
+import LoginComponent from "./routes/Login";
+import ScrollContextProvider from "./contexts/ScrollContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -12,32 +14,38 @@ const queryClient = new QueryClient({
       staleTime: 1000 * 10,
     },
   },
-})
+});
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      Component: Layout,
-      children: [
-        {
-          index: true,
-          loader: homeLoader(queryClient),
-          Component: HomeComponent,
-        },
-        {
-          path: "product/:id",
-          loader: productLoader(queryClient),
-          Component: Product,
-        },
-      ],
-    },
-  ],
-);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Layout,
+    children: [
+      {
+        index: true,
+        loader: homeLoader(queryClient),
+        Component: HomeComponent,
+      },
+      {
+        path: "login",
+        Component: LoginComponent,
+      },
+      {
+        path: "product/:id",
+        loader: productLoader(queryClient),
+        Component: ProductComponent,
+      },
+    ],
+  },
+]);
 
 export default function App() {
-  return <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
-    <ReactQueryDevtools buttonPosition="bottom-right" />
-  </QueryClientProvider>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ScrollContextProvider>
+        <RouterProvider router={router} />
+      </ScrollContextProvider>
+      <ReactQueryDevtools buttonPosition="bottom-right" />
+    </QueryClientProvider>
+  );
 }
