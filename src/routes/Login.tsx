0f37b268c9923c5b48c3e20f5145c9cda8230326
Spastic <junior.tan@live.com>
2024-02-23 06:56:28 +0800

    Update Login.tsx

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -2,6 +2,11 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { userSchema } from "@/lib/types";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card"
 
 import { Button } from "@/components/ui/button";
 import {
@@ -13,98 +18,112 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import useAuth from "@/hooks/useAuth";
 
-const loginFormSchema = userSchema.omit({
-  id: true,
-});
+const loginFormSchema = userSchema.pick({
+  password: true,
+  email: true,
+})
 
 export default function LoginComponent() {
+  const {state} = useLocation()
+  const navigate = useNavigate()
+  const { setUser } = useAuth()
   const form = useForm<z.infer<typeof loginFormSchema>>({
     resolver: zodResolver(loginFormSchema),
     defaultValues: {
-      avatar: "",
       email: "",
-      name: "",
       password: "",
-      username: "",
     },
   });
 
-  function onSubmit(values: z.infer<typeof loginFormSchema>) {
+  async function onSubmit(values: z.infer<typeof loginFormSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values);
+    // console.log(values);
+
+    const response = await fetch("https://api.escuelajs.co/api/v1/auth/login", {
+      method: "POST",
+      body: JSON.stringify(values),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+
+    if (response.ok) {
+      const {access_token, refresh_token} = await response.json()
+      localStorage.setItem('access_token', access_token)
+      localStorage.setItem('refresh_token', refresh_token)
+
+      const sessionResponse = await fetch("https://api.escuelajs.co/api/v1/auth/profile", {
+        headers: {
+          "Authorization": `Bearer ${access_token}`
+        }
+      })
+
+      if (sessionResponse.ok) {
+        const user = await sessionResponse.json()
+        setUser(user)
+        if (state?.from) return navigate(state.from)
+        navigate("/")
+      } 
+    }
   }
 
   return (
-    <section className="py-8">
-      <div className="container flex flex-wrap items-center max-w-screen-lg px-6 pt-4 pb-12 mx-auto lg:px-0">
-        <h2 className="mb-12 text-3xl font-bold tracking-tight text-black">
-          Login
-        </h2>
+    <>
+      <h2 className="mb-12 text-3xl font-bold tracking-tight text-black">
+        Login
+      </h2>
+      <Card className="w-full">
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
 
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Name</FormLabel>
-                  <FormControl>
-                    <Input placeholder="shadcn" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input type="email" placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Username</FormLabel>
-                  <FormControl>
-                    <Input placeholder="shadcn" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input type="password" placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input type="email" placeholder="shadcn" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </CardContent>
+        <CardFooter>
+          Do not have an account?
+          <Link
+            to="/register"
+            unstable_viewTransition
+            style={{ viewTransitionName: "login-register" }}
+          >Register</Link>
+        </CardFooter>
+      </Card>
 
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input type="password" placeholder="shadcn" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+    </>
 
-            <Button type="submit">Submit</Button>
-          </form>
-        </Form>
-      </div>
-    </section>
   );
 }
