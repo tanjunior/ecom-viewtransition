b7468def6ec2e71f09c5dd3ca8ae8f6fb7c51e6e
Spastic <junior.tan@live.com>
2024-02-22 23:34:58 +0800

    Update Home.tsx

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,6 +3,8 @@ import Card from "../components/Card";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { productsQuery } from "@/lib/queries";
 import { homeLoader } from "../lib/loaders";
+import { useEffect } from "react";
+import useScroll from "@/hooks/useScroll";
 
 export default function HomeComponent() {
   const { initialData } = useLoaderData() as Awaited<
@@ -13,18 +15,40 @@ export default function HomeComponent() {
     initialData,
   });
 
+  const {scrollPosition, setScrollPosition} = useScroll()
+  
+  const handleScroll = () => {
+      const position = window.scrollY;
+      setScrollPosition(position);
+  };
+  
+  useEffect(() => {
+      window.scrollTo({
+        top: scrollPosition,
+        behavior: "instant"
+      });
+
+      
+      window.addEventListener('scroll', handleScroll, { passive: true });
+  
+      return () => {
+          window.removeEventListener('scroll', handleScroll);
+      };
+  }, []);
+
   return (
-    <section className="py-8">
-      <div className="container flex flex-wrap items-center max-w-screen-lg px-6 pt-4 pb-12 mx-auto lg:px-0">
-        <h2 className="mb-12 text-3xl font-bold tracking-tight text-black">
-          Home
-        </h2>
-        <div className="grid w-full grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4">
-          {products.map((product) => (
-            <Card key={product.id} {...product} />
-          ))}
+      <section className="py-8">
+        <div className="container flex flex-wrap items-center max-w-screen-lg px-6 pt-4 pb-12 mx-auto lg:px-0">
+          <h2 className="mb-12 text-3xl font-bold tracking-tight text-black">
+            Home
+          </h2>
+          <div className="grid w-full grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4">
+            {products.map((product) => (
+              <Card key={product.id} {...product} />
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+        {/* <ScrollRestoration getKey={(location, matches) => location.pathname}/> */}
+      </section>
   );
 }
