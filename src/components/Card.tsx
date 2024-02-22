4cfa00769258350048a8b33120d9ad0b8ebae21d
Spastic <junior.tan@live.com>
2024-02-22 16:02:05 +0800

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,31 +1,27 @@
+import { Product } from "@/lib/types";
 import { NavLink } from "react-router-dom";
+import SuspenseImage from "./SuspenseImage";
+import { Suspense } from "react";
 
-type CardProps = {
-  id: string;
-  name: string;
-  artist: string;
-  imageUrl: string;
-};
-
-export function Card({ id, name, artist, imageUrl }: CardProps) {
+export default function Card({ id, category, description, images, price, title }: Product) {
   return (
     <div className="flex flex-col c-card">
       <NavLink
-        to={`/album/${id}`}
+        to={`/product/${id}`}
         className="text-black hover:text-pink-500"
         unstable_viewTransition
       >
         <div className="relative shadow-md hover:shadow-lg">
-          <img
-            className="relative z-10 rounded-md card-image c-card--album"
-            src={imageUrl}
-            alt={name}
-            width="400"
-            height="400"
-          />
+          <Suspense fallback={<div className="size-68">Loading image...</div>}>
+            <SuspenseImage
+              className="relative z-10 object-cover rounded-md card-image c-card--album size-68"
+              src={images[0]}
+              alt={title}
+            />
+          </Suspense>
         </div>
-        <p className="pt-4 font-semibold">{name}</p>
-        <p className="pt-1 text-gray-700">{artist}</p>
+        <p className="pt-4 font-semibold">{title}</p>
+        <p className="pt-1 text-gray-700">{price}</p>
       </NavLink>
     </div>
   );
