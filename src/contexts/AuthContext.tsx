cd9ff1d5e3805d24fed663dcffaed84dcada8faa
Spastic <junior.tan@live.com>
2024-02-23 06:54:59 +0800

    auth context

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,65 @@
+import {
+  ReactNode,
+  createContext,
+  useState,
+  type SetStateAction,
+  type Dispatch,
+  useEffect,
+} from "react";
+
+export type User = {
+  id: number;
+  name: string;
+  email: string;
+  role: string;
+  avatar: string;
+};
+
+type AuthContextType = {
+  user: User | null
+  setUser: Dispatch<SetStateAction<User | null>>;
+  logout: () => void;
+};
+
+export const AuthContext = createContext<AuthContextType | null>(null);
+
+export default function AuthContextProvider(props: { children: ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+  
+  useEffect( ()=>{
+    async function getSession() {
+      try {
+        const access_token = localStorage.getItem('access_token')
+        if(!access_token) return
+
+        const sessionResponse = await fetch("https://api.escuelajs.co/api/v1/auth/profile", {
+        headers: {
+          "Authorization": `Bearer ${access_token}`
+        }})
+        if (sessionResponse.ok) {
+          const user = await sessionResponse.json()
+          setUser(user)
+        } 
+      } catch(err: unknown) {
+        if (err instanceof Error) {
+          console.log(err.message)
+        }
+      }
+    }
+    getSession()
+  }, [])
+  
+  function logout() {
+
+    localStorage.removeItem('access_token')
+    localStorage.removeItem('refresh_token')
+    setUser(null)
+  }
+
+
+  return (
+    <AuthContext.Provider value={{ user, setUser, logout }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
