b05a81a02d22174a22d79e46ba4926c782daf066
Spastic <junior.tan@live.com>
2024-02-22 16:01:04 +0800

    move all loaders to a single file

---
+++ src/lib/loaders.ts
@@ -0,0 +1,16 @@
+import { productQuery, productsQuery } from "@/lib/queries"
+import { QueryClient } from "@tanstack/react-query"
+import { LoaderFunctionArgs } from "react-router-dom"
+
+
+export function homeLoader(queryClient: QueryClient) {
+  return async () => {
+    return {initialData: await queryClient.ensureQueryData(productsQuery())}
+  }
+}
+
+export function productLoader(queryClient: QueryClient) {
+  return async ({params}: LoaderFunctionArgs) => {
+    return {initialData: await queryClient.ensureQueryData(productQuery(params.id!))}
+  }
+}
