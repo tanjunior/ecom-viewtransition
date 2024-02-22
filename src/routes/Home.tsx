d841851ad41b0feaa92458e40d3258f5b64f70ee
Spastic <junior.tan@live.com>
2024-02-22 16:02:11 +0800

    Update Home.tsx

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,37 +1,27 @@
 import { useLoaderData } from "react-router-dom";
-import { Card } from "../components/Card";
-import { albumsQuery } from "../queries/album";
-import { QueryClient, useSuspenseQuery } from "@tanstack/react-query";
+import Card from "../components/Card";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { productsQuery } from "@/lib/queries";
+import { homeLoader } from "../lib/loaders";
 
-export function loader(queryClient: QueryClient) {
-  return async () => {
-    return {initialData: await queryClient.ensureQueryData(albumsQuery())}
-  }
-}
-
-export function Component() {
-  const {initialData} = useLoaderData() as Awaited<ReturnType<ReturnType<typeof loader>>>
-  const {data:albums} = useSuspenseQuery({
-    ...albumsQuery(),
-    initialData
-  })
+export default function HomeComponent() {
+  const { initialData } = useLoaderData() as Awaited<
+    ReturnType<ReturnType<typeof homeLoader>>
+  >;
+  const { data: products } = useSuspenseQuery({
+    ...productsQuery(),
+    initialData,
+  });
 
   return (
     <section className="py-8">
       <div className="container flex flex-wrap items-center max-w-screen-lg px-6 pt-4 pb-12 mx-auto lg:px-0">
         <h2 className="mb-12 text-3xl font-bold tracking-tight text-black">
-          Recently Played
+          Home
         </h2>
-
         <div className="grid w-full grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4">
-          {albums.map((album) => (
-            <Card
-              key={album.id}
-              id={album.id}
-              name={album.name}
-              artist={album.artist}
-              imageUrl={album.img}
-            />
+          {products.map((product) => (
+            <Card key={product.id} {...product} />
           ))}
         </div>
       </div>
