e4bbee6773770a17ab5e62de118cf4568062b959
Spastic <junior.tan@live.com>
2024-02-23 06:56:24 +0800

    Create Register.tsx

---
+++ src/routes/Register.tsx
@@ -0,0 +1,197 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { userSchema } from "@/lib/types";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const registerFormSchema = userSchema.omit({
+  id: true,
+  role: true,
+  avatar: true,
+}).extend({
+  confirmPassword: z.string()
+}).refine((value) => value.password === value.confirmPassword, {
+  path: ["confirmPassword"],
+  message: "Passwords do not match",
+});
+
+export default function RegisterComponent() {
+  const navigate = useNavigate()
+  const [avatar, setAvatar] = useState("")
+  useEffect(() => {
+    async function getRandomUserImage() {
+      const response = await fetch("https://randomuser.me/api/?inc=picture&noinfo").then(response => response.json())
+      setAvatar(response.results[0].picture.large)
+    }
+
+    getRandomUserImage()
+  }, [])
+
+
+  const form = useForm<z.infer<typeof registerFormSchema>>({
+    resolver: zodResolver(registerFormSchema),
+    defaultValues: {
+      email: "",
+      name: "",
+      password: "",
+      confirmPassword: "",
+      username: "",
+    },
+  });
+  
+
+  async function onSubmit(values: z.infer<typeof registerFormSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    // console.log({...values, avatar});
+
+    // const checkEmailResponse = await fetch("https://api.escuelajs.co/api/v1/users/is-available", {
+    //   method: "POST",
+    //   body: JSON.stringify({ email: values.email }),
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   }
+    // }).then(response => response.json())
+
+    // console.log(checkEmailResponse)
+    // if (!checkEmailResponse.isAvailable) return form.setError("email", { message: "Email is already taken" })
+    
+    const response = await fetch("https://api.escuelajs.co/api/v1/users/", {
+      method: "POST",
+      body: JSON.stringify({...values, avatar}),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+
+    if (response.ok) {
+      form.reset()
+      navigate("/login")
+    }
+    
+  }
+
+  return (
+    <>
+      <h2 className="mb-12 text-3xl font-bold tracking-tight text-black">
+        Register
+      </h2>
+      <Card className="w-full">
+        <CardHeader className="items-center justify-center">
+          <Avatar className="size-48">
+            <AvatarImage src={avatar}/>
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+
+            
+              
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="shadcn" {...field}/>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input type="email" placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input type="password" placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl>
+                      <Input type="password" placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </CardContent>
+        <CardFooter>
+          Already have an account?<Link
+            to="/login"
+            unstable_viewTransition
+            style={{ viewTransitionName: "register-login" }}
+          >Login</Link>
+        </CardFooter>
+      </Card>
+
+    </>
+
+  );
+}
