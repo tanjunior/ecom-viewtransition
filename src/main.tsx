0b1cc3a704975346fdc1d3485360d203b0bf0199
Spastic <junior.tan@live.com>
2024-02-23 06:56:56 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Toaster } from './components/ui/sonner.tsx'
+import AuthContextProvider from "./contexts/AuthContext";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+        <App />
+        <Toaster />
+    </AuthContextProvider>
   </React.StrictMode>,
 )
