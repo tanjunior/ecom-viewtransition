664cfe2b5940d1aff936951c772b714f1a57ddc5
Spastic <junior.tan@live.com>
2024-02-25 01:43:29 +0800

    Update AccountLayout.tsx

--- src/routes/AccountLayout.tsx
+++ src/routes/AccountLayout.tsx
@@ -1,26 +1,27 @@
-import Section from './Section'
-import { NavLink, Outlet } from 'react-router-dom'
+import { NavLink, Outlet, useNavigate } from 'react-router-dom'
 import useAuth from '@/hooks/useAuth'
 import { Button } from '@/components/ui/button'
 
 export default function AccountLayout() {
   return (
-    <Section>
-      <div className='flex w-full'>
-        <AccountSideBar />
-        <Outlet />
-      </div>
-    </Section>
+    <div className='flex w-full'>
+      <AccountSideBar />
+      <Outlet />
+    </div>
   )
 }
 
 
 function AccountSideBar() {
   const { logout} = useAuth()
+  const navigate = useNavigate()
   return (
     <div className='flex flex-col'>
         <NavLink to="/account">Profile</NavLink>
-        <Button onClick={() => logout()}>logout</Button>
+        <Button onClick={() => {
+          navigate("/")
+          logout()
+        }}>logout</Button>
     </div>
   )
 }
