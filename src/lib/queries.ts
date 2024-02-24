fbc7a9e03cabeebcadaac7717b02b2696e2b5d4b
Spastic <junior.tan@live.com>
2024-02-25 01:42:12 +0800

    Update queries.ts

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -1,43 +1,16 @@
-// import { FragmentOf, graphql } from "@/graphql/graphql";
-import { productSchema, productsSchema, usersSchema } from "@/lib/types";
+import { categoriesSchema, productSchema, productsSchema, usersSchema } from "@/lib/types";
 import { queryOptions } from "@tanstack/react-query";
-
-// import { Client, cacheExchange, fetchExchange } from 'urql';
-
-// const client = new Client({
-//   url: 'https://api.escuelajs.co/graphql',
-//   exchanges: [cacheExchange, fetchExchange],
-// });
-
-// const productsFragment = graphql(`
-//   fragment productsFragment on Product {
-//     id
-//     title
-//     price
-//     description
-//     images
-//     category {
-//       id
-//       name
-//       image
-//     }
-//   }
-// `)
-
-// export type Product = FragmentOf<typeof productsFragment>
 
 export const productsQuery = () =>
   queryOptions({
     queryKey: ["products"],
     queryFn: async () => {
+      const data = await fetch("https://fakestoreapi.com/products").then(
+        (res) => res.json()
+      );
 
-      // const data = await client.query(productsFragment, {}).toPromise().then(res => res.data)
-      // return data
-      const data = await fetch('https://fakestoreapi.com/products')
-      .then(res=>res.json())
-
-      const parse = productsSchema.safeParse(data)
-      return parse.success? parse.data : []
+      const parse = productsSchema.safeParse(data);
+      return parse.success ? parse.data : [];
     },
   });
 
@@ -45,11 +18,11 @@ export const productQuery = (id: string) =>
   queryOptions({
     queryKey: ["product", id],
     queryFn: async () => {
-      const response = await fetch(`https://fakestoreapi.com/products/${id}`)
-      if (!response.ok) return null
-      const data = await response.json()
-      const parse = productSchema.safeParse(data)
-      return parse.success ? parse.data : null
+      const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+      if (!response.ok) return null;
+      const data = await response.json();
+      const parse = productSchema.safeParse(data);
+      return parse.success ? parse.data : null;
     },
   });
 
@@ -57,12 +30,22 @@ export const usersQuery = () =>
   queryOptions({
     queryKey: ["users"],
     queryFn: async () => {
+      const data = await fetch("https://fakestoreapi.com/users").then((res) =>
+        res.json()
+      );
+      const parse = usersSchema.safeParse(data);
+      return parse.success ? parse.data : [];
+    },
+  });
 
-      // const data = await client.query(productsFragment, {}).toPromise().then(res => res.data)
-      // return data
-      const data = await fetch('https://fakestoreapi.com/users')
-      .then(res=>res.json())
-      const parse = usersSchema.safeParse(data)
-      return parse.success? parse.data : []
+export const categoriesQuery = () =>
+  queryOptions({
+    queryKey: ["categories"],
+    queryFn: async () => {
+      const data = await fetch("https://fakestoreapi.com/products/categories").then(
+        (res) => res.json()
+      );
+      const parse = categoriesSchema.safeParse(data);
+      return parse.success ? parse.data : [];
     },
   });
