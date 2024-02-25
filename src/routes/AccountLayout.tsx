835183f3cf04901257c0d7c9d681da74cb3204a4
Spastic <junior.tan@live.com>
2024-02-25 19:32:21 +0800

    Update AccountLayout.tsx

--- src/routes/AccountLayout.tsx
+++ src/routes/AccountLayout.tsx
@@ -11,13 +11,22 @@ export default function AccountLayout() {
   )
 }
 
+type NavLinkRenderProps = {
+  isActive: boolean;
+  isPending: boolean;
+  isTransitioning: boolean;
+};
 
 function AccountSideBar() {
   const { logout} = useAuth()
   const navigate = useNavigate()
+
+  const className = ({isActive}: NavLinkRenderProps) => isActive ? "text-primary" : ""
+  
   return (
     <div className='flex flex-col'>
-        <NavLink to="/account">Profile</NavLink>
+        <NavLink to={`/account/profile`} className={className}>Profile</NavLink>
+        <NavLink to={`/account/cart`} className={className}>Cart</NavLink>
         <Button onClick={() => {
           navigate("/")
           logout()
